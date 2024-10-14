2f4a5f4eef7f8a2ebdbaf88fb437182b46222b2c
harshika-soni <harshikamuz@gmail.com>
2024-10-14 07:02:44 -0700

    Update FlipkartExeTraversing.java

--- src/test/java/practice/FlipkartExeTraversing.java
+++ src/test/java/practice/FlipkartExeTraversing.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FlipkartExeTraversing {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+		//puling
 		 WebDriver driver= new ChromeDriver();
 		 driver.manage().window().maximize();
 		 
